599b9d9900805a0aa854abcf5e89135fd8c28bcc
Erick Amorim <erickamorimcarv@gmail.com>
2022-05-07 14:21:53 -0300

    Calculadora de IMC

--- 00-calculadora-imc/assets/js/main.js
+++ 00-calculadora-imc/assets/js/main.js
@@ -1,43 +1,41 @@
-/* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-  Capturando o Evento de Submit do Formulário HTML. -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
-
 const form = document.querySelector('#formulario');     // Formulário capturado e atribuído a varíavel form.
 
-/* -=-=-=-=-=-=-=-= Parando o Evento de Submit para Captura dos Dados de Entrada e Exibição dos Resultados. -=-=-=-=-=-=-=-= */
+/*  Parando o Evento de Submit para Captura dos Dados de Entrada e Exibição dos Resultados.  */
 
 form.addEventListener('submit', function (evento) {
     evento.preventDefault();
 
-    /* -=-=-=-= Pegando os Valores dos Inputs do Formulário. -=-=-=-= */
+    /*  Pegando os Valores dos Inputs do Formulário.  */
 
-    // Pegando os Inputs do HTML.
+    //  Pegando os Inputs do HTML.
     const inputPeso = evento.target.querySelector('#peso');
     const inputAltura = evento.target.querySelector('#altura');
 
-    // Pegando os Valores dos Inputs do HTML.
+    //  Pegando os Valores dos Inputs do HTML.
     const valorInputPeso = Number(inputPeso.value);
     const valorInputAltura = Number(inputAltura.value);
 
 
-    /* -=-=-=-= Calculando o Valor do IMC. -=-=-=-= */
+    /*  Calculando o Valor do IMC.  */
     const imc = calculaIMC(valorInputPeso, valorInputAltura);
 
 
-    /* -=-=-=-= Exibindo o Resultado do Cálculo do IMC. -=-=-=-=*/
+    /*  Exibindo o Resultado do Cálculo do IMC.  */
 
-    // Exibindo um Resultado Alternativo Caso os Valores dos Inputs não Sejam Números.
+    //  Exibindo um Resultado Alternativo Caso os Valores dos Inputs não Sejam Números.
     if ((!valorInputPeso) && (!valorInputAltura)) return exibeResultado('Peso e Altura Inválidos!', false)
     if (!valorInputPeso) return exibeResultado('Peso Inválido!', false);
     if (!valorInputAltura) return exibeResultado('Altura Inválida!', false);
 
-    // Exibindo o Resultado Caso os Valores dos Inputs Sejam Números.
+    //  Exibindo o Resultado Caso os Valores dos Inputs Sejam Números.
     exibeResultado(`Seu IMC vale ${imc}. Você está ${nivelIMC(imc)}`, true);
     
 });
 
 
-/* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= Seção das Funções. -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
+/*  Seção das Funções.  */
 
-// 1. Criando uma Função Especializada no Cálculo do IMC.
+//  1. Criando uma Função Especializada no Cálculo do IMC.
 
 function calculaIMC(peso, altura) {
     const imc = (peso / (altura ** 2));
@@ -45,7 +43,7 @@ function calculaIMC(peso, altura) {
 }
 
 
-// 2. Criando uma Função Especializada no Retorno de uma Mensagem Personalizada de Acordo com o IMC passado no Parâmetro.
+//  2. Criando uma Função Especializada no Retorno de uma Mensagem Personalizada de Acordo com o IMC passado no Parâmetro.
 
 function nivelIMC(imc) {
     const nivel = ['Abaixo do Peso', 'Peso Normal', 'Sobrepeso', 'Obesidade Grau I', 'Obesidade Grau II', 'Obesidade Mórbida']
@@ -59,7 +57,7 @@ function nivelIMC(imc) {
 }
 
 
-// 3. Criando uma Função Especializada na Criação de Parágrafos.
+//  3. Criando uma Função Especializada na Criação de Parágrafos.
 
 function criaParagrafo() {
     const paragrafo = document.createElement('p');  // Criando uma tag <p> na memória e atribuindo a variável paragrafo.
@@ -67,7 +65,7 @@ function criaParagrafo() {
 }
 
 
-// 4. Criando uma Função Especializada na Exibição do Resultado dos cálculos na Tela.
+//  4. Criando uma Função Especializada na Exibição do Resultado dos cálculos na Tela.
 
 function exibeResultado(mensagem, valido) {
 
